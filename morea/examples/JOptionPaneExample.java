9c03bff842a70c5f0b6d03161c07471e2d29afa0
William Albritton <walbritt@hawaii.edu>
2014-08-26 15:46:32 -1000

    added ADT module

--- morea/examples/JOptionPaneExample.java
+++ morea/examples/JOptionPaneExample.java
@@ -1,22 +1,26 @@
-   import javax.swing.JOptionPane;
-/** 
- *  @author William Albritton 
- *  Example of JOptionPane and toString() method
- *  Must be compiled with Fraction.java file in the same folder
- */
-   public class JOptionPaneExample {
-   /**main() method: begins program
-    * @param arguments is not used */
-      public static void main(String[ ] args) {
-         JOptionPane.showMessageDialog(null, "Here is example output using JOptionPane");  
-         JOptionPane.showMessageDialog(null, "Here is a second output statement using JOptionPane");
-         
-      	//example of toString() for class Fraction
-      	Fraction fraction1 = new Fraction(1,2);
-      	String display = fraction1.toString();
-      	JOptionPane.showMessageDialog(null, "fraction1 is " + display);
-      	System.out.println("The toString() output for fraction1 is " + display);
-      }
-   }//end of class
+import javax.swing.JOptionPane;
 
+/**
+ * Example of JOptionPane and toString() method. Must be compiled with Fraction.java file in the
+ * same folder
+ * 
+ * @author William Albritton
+ */
+public class JOptionPaneExample {
+  /**
+   * main() method: begins program
+   * 
+   * @param arguments is not used
+   */
+  public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null, "Here is example output using JOptionPane");
+    JOptionPane.showMessageDialog(null, "Here is a second output statement using JOptionPane");
+
+    // example of toString() for class Fraction
+    Fraction fraction1 = new Fraction(1, 2);
+    String display = fraction1.toString();
+    JOptionPane.showMessageDialog(null, "fraction1 is " + display);
+    System.out.println("The toString() output for fraction1 is " + display);
+  }
+}// end of class
 
--- morea/examples/NullPointerProgram.java
+++ morea/examples/NullPointerProgram.java
@@ -1,20 +1,23 @@
 /**
-* Demonstrates a NullPointerException
-* @author William McDaniel Albritton
-*/
-    public class NullPointerProgram{
-      
-   /**main() method: begins program
-    * @param arguments is not used */
-       public static void main(String[] arguments) {
-      
-         Integer x = new Integer(5); //initialize variable
-         x = null; //set the address to nothing
-         String str = x.toString(); //throws NullPointerException
-         System.out.println(str); 
-            
-      }//End of Main.
-     
-   }//End of Class.
-   
-	
+ * Demonstrates a NullPointerException
+ * 
+ * @author William McDaniel Albritton
+ */
+public class NullPointerProgram {
+
+  /**
+   * main() method: begins program
+   * 
+   * @param arguments is not used
+   */
+  public static void main(String[] arguments) {
+
+    Integer x = new Integer(5); // initialize variable
+    x = null; // set the address to nothing
+    String str = x.toString(); // throws NullPointerException
+    System.out.println(str);
+
+  }// End of Main.
+
+}// End of Class.
+
